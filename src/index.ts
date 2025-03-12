83a8f9e6afe7cd9fec536edf146fdc841b2a6657
Chihiro88G <1114chihiro@gmail.com>
2025-03-12 09:03:57 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+/// <reference types="@types/google.maps" />
+
+import { Company } from './Company';
+import { User } from './User';
+
+const user = new User();
+console.log(user);
+
+const company = new Company();
+console.log(company);
+
+console.log(google);
+
+const map = new google.maps.Map(document.getElementById('map')!, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+});
